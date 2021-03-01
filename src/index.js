cd8a9eb7e53bc23f16e575e863767a4dc34c70c8
Ivan Ivanov <ivanov-i.s@mail.ru>
2021-03-01 15:37:25 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairExist = chars => {
+    let exist = false;
+    for (let i=0; i<bracketsConfig.length; i++){
+      if (chars[0] === bracketsConfig[i][0] && chars[1] === bracketsConfig[i][1]) {
+        exist = true;
+        break;
+      };
+    }
+    return exist;
+  }
+
+  if (str.length % 2 === 0) {
+    let i = 0;
+    while (i < str.length-1) {
+      if (pairExist(str[i]+str[i+1])) {
+        str = str.substring(0,i) + str.substring(i+2);
+        i = 0;
+      } else i++;
+    } 
+  } else return false;
+
+  if (str.length === 0) return true;
+  else return false; 
 }
